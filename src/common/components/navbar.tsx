c056f3daa060b05887b1351532e50cbda8ff2574
Nick Goble <ngoble@Nick-Goble.local>
2020-06-01 13:09:41 -0500

    shorthand syntax for navbar

--- src/common/components/navbar.tsx
+++ src/common/components/navbar.tsx
@@ -4,21 +4,19 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { resume } from "../constants/routes";
 
-const NavBar = () => {
-  return (
-    <header>
-      <Navbar expand="md" variant="dark" collapseOnSelect>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto" defaultActiveKey="/resume">
-            <Nav.Link as={Link} to={resume} eventKey="/resume">
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </header>
-  );
-};
+const NavBar = () => (
+  <header>
+    <Navbar expand="md" variant="dark" collapseOnSelect>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto" defaultActiveKey="/resume">
+          <Nav.Link as={Link} to={resume} eventKey="/resume">
+            Resume
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </header>
+);
 
 export default NavBar;
