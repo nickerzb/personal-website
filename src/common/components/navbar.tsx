8b7e1b4d2b66f80555f925850d7d70c6c57fad6c
Nick Goble <ngoble@phdata.io>
2020-08-25 19:54:03 -0500

    Fixed issue with nav not highlighting on refresh

--- src/common/components/navbar.tsx
+++ src/common/components/navbar.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { resume, about, home } from "../constants/routes";
-import Button from "react-bootstrap/Button";
+import { resume, about } from "../constants/routes";
 import "./navbar-styles.scss";
 import NavLink from "react-bootstrap/NavLink";
+import Button from "react-bootstrap/Button";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavBar = () => (
   <header>
     <Navbar expand="md" variant="dark" collapseOnSelect>
-      <Navbar.Brand id="brand-logo" as={Button} href={`/#${home}`}>
+      <Navbar.Brand id="brand-logo" as={Button} href={`#/`}>
         N/G
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" defaultActiveKey={resume}>
-          <Nav.Link as={NavLink} to={resume} eventKey={resume}>
-            Resume
-          </Nav.Link>
-          <Nav.Link as={NavLink} to={about} eventKey={about}>
-            About
-          </Nav.Link>
+        <Nav>
+          <LinkContainer to={resume}>
+            <NavLink>Resume</NavLink>
+          </LinkContainer>
+          <LinkContainer to={about}>
+            <NavLink>About</NavLink>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
