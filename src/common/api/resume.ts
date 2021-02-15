fcc5e782171195b58c9916ddc70139ddfc27b5cd
Nick Goble <ngoble@phdata.io>
2021-02-15 13:59:49 -0700

    Removed local JSON files and read from cosmos db

--- src/common/api/resume.ts
+++ src/common/api/resume.ts
@@ -1,19 +1,33 @@
-//import { getAxiosInstance } from './axios-instance'
-import jobData from "./jobs.json";
-import certificationData from "./certifications.json";
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = "https://personal-website-db.documents.azure.com/"; // Add your endpoint
+const key =
+  "pj8SpKm1HRA8PHpQ3d4qNXEUxcYjqUJ7d2HcKH0nU9F15P0POtjwgVyR8XP4PP1rViKHPfh0He1zegljWCbHpw=="; // Add the masterkey of the endpoint
+const client = new CosmosClient({ endpoint, key });
 
 export const getJobInfo = async (): Promise<Job[]> => {
-  let data: Job[] = (jobData as any) as Job[];
-  data = data.sort((a, b) => {
-    const aDate = +new Date(a.start_date);
-    const bDate = +new Date(b.start_date);
-    return bDate - aDate;
-  });
-  return data;
+  const database = client.database("Personal-Website");
+  const container = database.container("Jobs");
+
+  const querySpec = {
+    query: "SELECT * from jobs order by jobs.start_date desc"
+  };
+  const { resources: items } = await container.items
+    .query(querySpec)
+    .fetchAll();
+  return items as Job[];
 };
 
 export const getCertificationsInfo = async (): Promise<Certification[]> => {
-  let data: Certification[] = (certificationData as any) as Certification[];
-  data = data.sort((a, b) => a.title.localeCompare(b.title));
-  return data;
+  const database = client.database("Personal-Website");
+  const container = database.container("Certifications");
+
+  const querySpec = {
+    query: "SELECT * from certifications order by certifications.title"
+  };
+  const { resources: items } = await container.items
+    .query(querySpec)
+    .fetchAll();
+
+  return items as Certification[];
 };
